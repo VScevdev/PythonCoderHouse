929c44ceaf9c743674e4bcc543703979e7101704
Valentin Scevola <valentinscevolaaste@gmail.com>
2025-11-26 17:31:19 -0300

    Repositorio limpiado

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="paquete1",
-    version="1.0",
-    packages=find_packages(),
-    description="Modelo de clientes",
-    author="Valentín Scevola",
-)
